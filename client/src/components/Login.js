d57d2a715232fa14eeda912fdf7bc8e4f6747735
Dani Tacheny <dani.tacheny@gmail.com>
2018-11-21 14:37:41 -0600

    add form and state to Login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
   render() {
-    return <div>Login works!</div>;
+    return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" value={this.state.email} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" value={this.state.password} />
+        </div>
+      </form>
+    </div>);
   }
 }
 
