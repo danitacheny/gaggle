94bfb7adb72f4355145fe1981d171b2a0e8c4bf6
Dani Tacheny <dani.tacheny@gmail.com>
2018-11-21 14:41:42 -0600

    add handleChange method to Login for updating the form fields on state

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,17 +6,22 @@ class Login extends React.Component {
     password: '',
   }
 
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
     <div>
       <form>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email" value={this.state.email} />
+          <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" value={this.state.password} />
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
         </div>
         <button>Login</button>
       </form>
