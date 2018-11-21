975a5feaf2388ffae0dafd125b0566fbb7c61362
Dani Tacheny <dani.tacheny@gmail.com>
2018-11-21 14:58:04 -0600

    add updateState method to App component and pass into Login

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 import Login from './components/Login';
 
 class App extends Component {
+  state = {
+    user: '',
+  }
+
+  updateState = (key, value) => {
+    this.setState({ [key]: value });
+  }
+
   render() {
     return (
       <div>
-        <Login />
+        <Login updateLogin={this.updateState} />
+        {this.state.user}
       </div>
     );
   }
