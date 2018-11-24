4c36f5c326165002eb3e9428f0dd003a5529a1df
Dani Tacheny <dani.tacheny@gmail.com>
2018-11-23 19:45:29 -0600

    add Form.module.css for login and register components

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import style from './styles/Form.module.css';
+
 class Login extends React.Component {
   state = {
     email: '',
@@ -10,7 +12,7 @@ class Login extends React.Component {
 
   handleChange = event => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value, error: '' });
   };
 
   handleSubmit = event => {
@@ -20,37 +22,52 @@ class Login extends React.Component {
         error: 'Please fill out both email and password fields!',
       });
     } else {
+      // TODO add axios call
       this.props.updateAuth(true, this.state.email);
+      this.props.history.push('/');
     }
   };
 
   render() {
     return (
-      <div>
-        <div>{this.state.error}</div>
-        <form onSubmit={this.handleSubmit}>
-          <div>
+      <div className={style.container}>
+        <header>
+          <h1>Login:</h1>
+        </header>
+        {this.state.error && (
+          <div className={style.error}>{this.state.error}</div>
+        )}
+        <form onSubmit={this.handleSubmit} className={style.form}>
+          <div className={style.formGroup}>
             <label htmlFor="email">Email:</label>
             <input
               type="email"
               name="email"
               value={this.state.email}
               onChange={this.handleChange}
+              placeholder="user@email.com"
+              className={style.input}
             />
           </div>
-          <div>
-            <label htmlFor="password">Password</label>
+          <div className={style.formGroup}>
+            <label htmlFor="password">Password:</label>
             <input
               type="password"
               name="password"
               value={this.state.password}
               onChange={this.handleChange}
+              className={style.input}
             />
           </div>
-          <button type="submit">Login</button>
+          <div className={style.buttonGroup}>
+            <button type="submit" className={style.button}>
+              Login
+            </button>
+            <Link to="register">
+              <button className={style.button}>Register</button>
+            </Link>
+          </div>
         </form>
-        Don't have an account?
-        <Link to='register'><button>Register a New Account</button></Link>
       </div>
     );
   }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+
+import style from './styles/Form.module.css';
 
 class Register extends React.Component {
   state = {
@@ -7,7 +9,7 @@ class Register extends React.Component {
     password: '',
     confirmPassword: '',
     error: '',
-  }
+  };
 
   handleChange = event => {
     const { name, value } = event.target;
@@ -16,54 +18,74 @@ class Register extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    if (!this.state.email || !this.state.password || !this.state.confirmPassword) {
+    if (
+      !this.state.email ||
+      !this.state.password ||
+      !this.state.confirmPassword
+    ) {
       this.setState({
         error: 'Please fill out all fields!',
       });
+      return;
     } else if (this.state.password !== this.state.confirmPassword) {
       this.setState({ error: 'Passwords do not match' });
+      return;
     }
     // TODO: add axios call here
+    this.props.history.push('login');
   };
 
   render() {
     return (
-      <div>
-        <div>{this.state.error}</div>
-        <form onSubmit={this.handleSubmit}>
-          <div>
+      <div className={style.container}>
+        <header>
+          <h1>Register:</h1>
+        </header>
+        {this.state.error && (
+          <div className={style.error}>{this.state.error}</div>
+        )}
+        <form onSubmit={this.handleSubmit} className={style.form}>
+          <div className={style.formGroup}>
             <label htmlFor="email">Email:</label>
             <input
               type="email"
               name="email"
               value={this.state.email}
               onChange={this.handleChange}
+              className={style.input}
             />
           </div>
-          <div>
+          <div className={style.formGroup}>
             <label htmlFor="password">Password</label>
             <input
               type="password"
               name="password"
               value={this.state.password}
               onChange={this.handleChange}
+              className={style.input}
             />
           </div>
-          <div>
+          <div className={style.formGroup}>
             <label htmlFor="confirmPassword">Confirm Password</label>
             <input
               type="password"
               name="confirmPassword"
               value={this.state.confirmPassword}
               onChange={this.handleChange}
+              className={style.input}
             />
           </div>
-          <button type="submit">Register</button>
+          <div className={style.buttonGroup}>
+            <button type="submit" className={style.button}>
+              Register
+            </button>
+            <Link to="login">
+              <button className={style.button}>Login</button>
+            </Link>
+          </div>
         </form>
-        Already have an account?
-          <Link to='login'><button>Login</button></Link>
       </div>
-    )
+    );
   }
 }
 
